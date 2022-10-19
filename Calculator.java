0f84448c8c2c0673574e922e897cd732c028b630
Salman-003 <dsalman62@gmail.com>
2022-10-19 18:06:02 +0530

    add function added

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,12 @@
 public class Calculator{
+	
+	public void add(int num1, int num2) {
+		int result = num1 + num2;
+		System.out.println(result);
+	}
+	
 	public static void main(String[] args){
-
+		Calculator calc=new Calculator();
+		calc.add(5, 6);
 	}
 }
