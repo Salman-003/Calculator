098e2223651aaa46141166b7ff2a02bf5f7dc9ef
rajesh <rajumohan1998@gmail.com>
2022-10-19 18:21:56 +0530

    Added sub function

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,19 @@
 public class Calculator{
 	
 	public void add(int num1, int num2) {
-		int result = num1 + num2;
-		System.out.println(result);
+		int addResult = num1 + num2;
+		System.out.println(addResult);
+	}
+	
+	public void sub(int a, int b)
+	{
+		int subResult = b-a;
+		System.out.println(subResult);
 	}
 	
 	public static void main(String[] args){
 		Calculator calc=new Calculator();
 		calc.add(5, 6);
+		calc.sub(20, 10);
 	}
 }
